3b616a263396c900dcb54e82f1bf546b7b6fc5b3
unknown <guirat.faten.gf@gmail.com>
2022-11-14 15:31:26 +0100

    test commit

---
+++ src/main/java/tn/devops/entities/Teacher.java
@@ -0,0 +1,7 @@
+package tn.devops.entities;
+public class Teacher extends Person{
+
+    public Teacher(String name, int age, String address, String email) {
+        super(name, age, address, email);
+    }
+}
