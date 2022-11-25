2ac651db11e278c9e7616dd2687ae8e90a57a305
ChaimaAraibi <chaima.araibi@insat.u-carthage.tn>
2022-11-25 12:22:55 +0100

    completed department and teacher

--- src/main/java/tn/devops/entities/Department.java
+++ src/main/java/tn/devops/entities/Department.java
@@ -1,2 +1,44 @@
-package tn.devops.entities;public class Department {
+package tn.devops.entities;
+
+import javax.persistence.OneToMany;
+import java.util.*;
+
+public class Department {
+
+    private int departmentId;
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Student> students;
+
+    @OneToMany(mappedBy = "department")
+    private List<Teacher> teachers;
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setId(int id) {
+        this.departmentId = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String deptName) {
+        this.name = deptName;
+    }
+
+    public Collection<Student> getStudents() {
+        return (Collection<Student>) students;
+    }
+
+    public Collection<Teacher> getTeachers() {
+        return (Collection<Teacher>) teachers;
+    }
+
+
 }
+
--- src/main/java/tn/devops/entities/Teacher.java
+++ src/main/java/tn/devops/entities/Teacher.java
@@ -3,19 +3,64 @@ package tn.devops.entities;
 import java.util.Collection;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 
 @Entity
-public class Teacher extends Person{
-
-    public Teacher(String name, int age, String address, String email) {
-        super(name, age, address, email);
-    }
-
+public class Teacher {
+	
+	private int teacherId;
+    private String name;
+    private String address;
+    private String email;
+    @ManyToOne
+    private Department department;
     @OneToMany(mappedBy = "teacher")
     private Collection<Research> researches;
 
+
+    public Teacher(String name, int age, String address, String email) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getTeacherName() {
+        return name;
+    }
+    
+	public String getTeacherAddress() {
+        return address;
+    }
+    
+	public String getTeacherEmail() {
+        return email;
+    }
+    
+	public int getTeacherId() {
+        return teacherId;
+    }
+	
+	public int getTeacherDepartment() {
+        return department.getDepartmentId();
+    }
+	
+	public void setTeacherName(String name) {
+		this.name = name;
+	}
+	
+	public void setTeacherEmail(String name) {
+		this.name = name;
+	}
+	
+	public void setTeacherAddress(String name) {
+		this.name = name;
+	}
+	
+	
+    
+    
     //Research 
     /*
     @ManyToOne
