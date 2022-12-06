8e00608298c2c5b06dcadb2900dd85a97b83e5bc
Fatou99 <guirat.faten.gf@gmail.com>
2022-12-06 14:55:11 +0100

    main class

--- src/main/java/com/example/devopsproject/DevOpsProjectApplication.java
+++ src/main/java/com/example/devopsproject/DevOpsProjectApplication.java
@@ -1,13 +1,13 @@
-/*package com.example.devopsproject;
+package com.example.devopsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DevOpsProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevOpsProjectApplication.class, args);
-	}
+	//public static void main(String[] args) {
+//		SpringApplication.run(DevOpsProjectApplication.class, args);
+//	}
 
-}*/
+}
